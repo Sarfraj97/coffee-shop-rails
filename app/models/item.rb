bd7104c6c99dc17762bc47a3e09188bc17ddc6a4
shreesh1903 <shvyas@bestpeers.com>
2023-07-25 18:55:50 +0530

    reupdate volume type

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,17 @@
 # frozen_string_literal: true
 
 class Item < ApplicationRecord
+  extend Enumerize
+
+  enumerize :volume_type, in: { kg: 0, ltr: 1, ml: 2 }, predicates: true
+
+  validates :volume_type, presence: true
+
   TAX_RATE = 2
+
   validates :name, presence: true
   validates :price, presence: true
-  validates :volume_type, presence: true
+
 
   has_many :order_items, dependent: :destroy
   has_many :orders, through: :order_items
@@ -16,11 +23,5 @@ class Item < ApplicationRecord
   def tax_amount
     self.price * TAX_RATE / 100
   end
-
-  enum volume_type: {
-    kg: 0,
-    ltr: 1,
-    ml: 2
-  }
 
 end
